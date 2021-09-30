cb9cfeed4ec86ebda002d4e5f9dab9da164631af
Stenger2 <manyukvova@mail.ru>
2021-09-30 14:00:20 +0300

    first changer

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,53 +10,94 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-          
+            
+
+           
             int i;
-            int res = 0;
-            int res2 = 0;
-            int d = 0;
-            char x = 'X';
+            int sum = 0;
 
-            int[] a = new int[9];
-            int length = a.Length;
-            Console.WriteLine("Введите первые 9 чисел ISBN: ");
-            for (i = 0; i < 9; i++)
+            Console.WriteLine("Введите размер массива: ");
+            i = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[i];
+            Console.WriteLine("Введите элементы массива: ");
+
+            for (i = 0; i < arr.Length; i++)
             {
-                a[i] = int.Parse(Console.ReadLine());
-                res += a[i] * length;
-                length--;
+                arr[i] = int.Parse(Console.ReadLine());  
             }
 
-            res2 = res;
+            int max = arr.Max();
+            int min = arr.Min();
 
-            while (d<10)
+            int MaxInd = Array.IndexOf(arr, max);
+            int MiniInd = Array.IndexOf(arr, min);
+
+            for (i = MiniInd; i <= MaxInd; i++)
             {
-                res = res2;
-                res += d * 1;
-                if (res % 11 == 0)
-                {
-                    
-                    break;
-                }
-                d++;
-
+                sum += arr[i]; 
             }
 
-            for (i = 0; i < 9; i++)
-            {
-                Console.Write(a[i] + "");
+            
 
-            }
-            if (d == 10)
+            Console.WriteLine("Ваш массив:");
+
+            for (i = 0; i < arr.Length; i++)
             {
-                Console.WriteLine(x);
+                Console.WriteLine(arr[i] + "");
             }
-            else
-            {
-                Console.WriteLine(d);
-            }
-          
+
+            
+            Console.WriteLine("Сумма:" + sum);
+
             Console.ReadKey();
+
+            /*
+              int i;
+              int res = 0;
+              int res2 = 0;
+              int d = 0;
+              char x = 'X';
+
+              int[] a = new int[9];
+              int length = a.Length;
+              Console.WriteLine("Введите первые 9 чисел ISBN: ");
+              for (i = 0; i < 9; i++)
+              {
+                  a[i] = int.Parse(Console.ReadLine());
+                  res += a[i] * length;
+                  length--;
+              }
+
+              res2 = res;
+
+              while (d<10)
+              {
+                  res = res2;
+                  res += d * 1;
+                  if (res % 11 == 0)
+                  {
+
+                      break;
+                  }
+                  d++;
+
+              }
+
+              for (i = 0; i < 9; i++)
+              {
+                  Console.Write(a[i] + "");
+
+              }
+              if (d == 10)
+              {
+                  Console.WriteLine(x);
+              }
+              else
+              {
+                  Console.WriteLine(d);
+              }
+
+            */
         }
  
     }
